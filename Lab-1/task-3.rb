3770a10f2b68aae0a7cf323bb48e465d8525308f
Kseniia <kbasloviak@gmail.com>
2021-02-23 23:25:29 +0300

    task3v3(with case)

--- Lab-1/task-3.rb
+++ Lab-1/task-3.rb
@@ -1,8 +1,15 @@
 puts "Какой твой любимый язык?"
 language = gets.strip
 
-unless language.downcase != "ruby" then
+case language.downcase
+when "ruby"
 	puts "Ты подлиза!"
-else 
-	puts "Скоро будет Ruby! #{language} тебе больше не нужен!"
+when "pascal"
+	puts "Да ты шутник! Скоро будет Ruby!"
+when "python"
+	puts "Отлично! Скоро будет Ruby!"
+when "c++"
+	puts "Не беспокойся! Скоро будет Ruby!"
+else
+	puts "#{language}? Такого языка я не знаю. Но всё равно твоим любимым скоро будет Ruby!"
 end
