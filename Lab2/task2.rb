695231aaa2e51aa23e984f4cb80eb6a1ffe2e02d
Kseniia <kbasloviak@gmail.com>
2021-03-13 14:25:04 +0300

    task2 without multiplication

---
+++ Lab2/task2.rb
@@ -0,0 +1,10 @@
+puts "Input numbers:"
+list = gets.strip.split
+
+newList = Array.new
+list.each{|x| newList.append(x.to_i)}
+
+puts "Min: #{newList.min}"
+puts "Max: #{newList.max}"
+puts "Sum: #{newList.sum}"
+#TOODO multiplication puts "Mult: #{newList.???}"
