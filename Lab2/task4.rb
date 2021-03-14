9f6cb40f2f3b3c8b4aafa02d76710d3d7f323b9b
Kseniia <kbasloviak@gmail.com>
2021-03-14 16:06:03 +0300

    task5

--- Lab2/task4.rb
+++ Lab2/task4.rb
@@ -1,7 +1,30 @@
+command = ARGV[0]
+
 def readFile(path)
 	file = File.open(path)
 	list = file.read.strip.split.map{|x| x.to_i}
 end
 
-path = "task4.txt"
-p readFile(path)
+def readList(list, amount)
+	amount.times do |i|
+		list.insert(i, STDIN.gets.to_i)
+	end
+end
+
+
+case command
+when "-h"
+	puts "Способы ввода списка:\n1 n - Ввод вручную, где n - количество элементов списка
+		\n2 path - Ввод из файла, где path - путь к файлу"
+when "1"
+	amount = ARGV[1].to_i
+	puts "Input numbers:"
+	list = []
+	readList(list, amount)
+	p list
+when "2"
+	path = ARGV[1]
+	p readFile(path)
+else
+	puts "Такого метода нет. Для справки ввести -h"
+end
