fc63815e1638d50f77ca792a15f702825020f95c
Kseniia <kbasloviak@gmail.com>
2021-03-28 21:50:38 +0300

    add some changes

--- Lab2/task6.rb
+++ Lab2/task6.rb
@@ -12,7 +12,7 @@ def task3 list, index
 		return false
 	end
 	list.length.times do |i|
-		if list[i] >= list[index] then
+		if list[i] > list[index] then
 			return false
 		end
 	end
@@ -27,13 +27,13 @@ end
 =end
 
 def task15 list, index
-	if list.length - 1 < index || index < 0 then
+	if list.length - 2 < index || index < 1 then
 		puts "Неверный индекс"
 		return false
 	end
 	index_left = list[index] < list[index-1]
 	index_right = list[index] < list[index+1]
-	index_left == index_right
+	index_left == index_right && index_left == true
 end
 
 =begin
@@ -59,9 +59,9 @@ def task39 list
 	even = []
 	odd = []
 	(list.length).times do |i|
-		list[i].even? ? even.push(list[i]) : odd.push(list[i])
+		i.even? ? even.push(list[i]) : odd.push(list[i])
 	end
-	"#{even} #{odd}"
+	"#{odd} #{even}"
 end
 
 =begin
@@ -85,4 +85,44 @@ def task51 list
 	end
 	p list1
 	p list2
+end
+
+command = ARGV[0]
+path = ARGV[1]
+file = File.open(path)
+list = file.readline.split.map{|x| x.to_i}
+
+case command
+when "-h"
+	puts "Методы:\n1 Определит является ли элемент по
+указанному индексу глобальным максимумом.
+		\n2 Определит является ли элемент по
+указанному индексу локальным минимумом.
+		\n3 Осуществляет циклический
+сдвиг элементов массива влево на одну позицию.
+		\n4 Вывод вначале
+элементы с четными индексами, а затем - с нечетными.
+		\n5 Для введенного списка построить два списка L1 и L2, где элементы L1
+это неповторяющиеся элементы исходного списка, а элемент списка L2 с
+номером i показывает, сколько раз элемент списка L1 с таким номером
+повторяется в исходном."
+when "1"
+	index = file.readline.to_i
+	p list, index
+	p task3 list, index
+when "2"
+	index = file.readline.to_i
+	p list, index
+	p task15 list, index
+when "3"
+	p list
+	p task27 list
+when "4"
+	p list
+	p task39 list
+when "5"
+	p list
+	task51 list
+else
+	puts "Такого метода нет. Для справки ввести -h"
 end
