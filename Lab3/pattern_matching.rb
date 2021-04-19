e7ca278a1061561d329093169399f81013e4d118
Kseniia <kbasloviak@gmail.com>
2021-04-19 04:01:11 +0300

    prttern matching for email

--- Lab3/pattern_matching.rb
+++ Lab3/pattern_matching.rb
@@ -11,4 +11,14 @@ module PatternMatching
     raise ArgumentError, "Некорректный номер!" unless is_phone_number? phone
     phone.gsub('-', '').gsub('(', '').gsub(')', '').gsub(' ', '').gsub('+7', '8').insert(1, '-').insert(5, '-')
   end
+
+  def self.is_email?(email)
+    (/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/ =~ email) != nil
+  end
+
+  def self.convert_email(email)
+    raise ArgumentError, 'Некорректный email' unless is_email? email
+    email.downcase
+  end
+
 end
